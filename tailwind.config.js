7edc4c959516ae0eb001d35616711e25169e485f
joshiaayush <76644638+joshiaayush@users.noreply.github.com>
2023-09-14 22:28:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        13: '3.25rem'
+      },
+      fontSize:{
+        '10xl': ['9rem', { lineHeight: '1' }]
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
